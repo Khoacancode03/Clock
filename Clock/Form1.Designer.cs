52838a94e9a70554ce2bce312c45e9f0b4825b8d
Khoa <khoaknhe170164@fpt.edu.vn>
2022-08-25 22:32:14 +0700

    Add project files.

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbHour = new System.Windows.Forms.Label();
+            this.lbMinute = new System.Windows.Forms.Label();
+            this.lbSecond = new System.Windows.Forms.Label();
+            this.lbTik = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbHour
+            // 
+            this.lbHour.AutoSize = true;
+            this.lbHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.8F);
+            this.lbHour.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbHour.Location = new System.Drawing.Point(133, 140);
+            this.lbHour.Name = "lbHour";
+            this.lbHour.Size = new System.Drawing.Size(63, 69);
+            this.lbHour.TabIndex = 0;
+            this.lbHour.Text = "0";
+            // 
+            // lbMinute
+            // 
+            this.lbMinute.AutoSize = true;
+            this.lbMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.8F);
+            this.lbMinute.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbMinute.Location = new System.Drawing.Point(279, 140);
+            this.lbMinute.Name = "lbMinute";
+            this.lbMinute.Size = new System.Drawing.Size(96, 69);
+            this.lbMinute.TabIndex = 1;
+            this.lbMinute.Text = "00";
+            // 
+            // lbSecond
+            // 
+            this.lbSecond.AutoSize = true;
+            this.lbSecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.8F);
+            this.lbSecond.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbSecond.Location = new System.Drawing.Point(471, 140);
+            this.lbSecond.Name = "lbSecond";
+            this.lbSecond.Size = new System.Drawing.Size(96, 69);
+            this.lbSecond.TabIndex = 2;
+            this.lbSecond.Text = "00";
+            // 
+            // lbTik
+            // 
+            this.lbTik.AutoSize = true;
+            this.lbTik.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.8F);
+            this.lbTik.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbTik.Location = new System.Drawing.Point(626, 140);
+            this.lbTik.Name = "lbTik";
+            this.lbTik.Size = new System.Drawing.Size(63, 69);
+            this.lbTik.TabIndex = 3;
+            this.lbTik.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(145, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 80);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(378, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.btnStart.Location = new System.Drawing.Point(408, 260);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(201, 80);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.CornflowerBlue;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(215, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(414, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(587, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(179, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "hours";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(354, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "minutes";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(545, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "secs";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(670, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "1/10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbTik);
+            this.Controls.Add(this.lbSecond);
+            this.Controls.Add(this.lbMinute);
+            this.Controls.Add(this.lbHour);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbHour;
+        private System.Windows.Forms.Label lbMinute;
+        private System.Windows.Forms.Label lbSecond;
+        private System.Windows.Forms.Label lbTik;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int hour = Convert.ToInt32(lbHour.Text);
+            int minute = Convert.ToInt32(lbMinute.Text);
+            int second = Convert.ToInt32(lbSecond.Text);
+            int tik = Convert.ToInt32(lbTik.Text);
+          
+            //Start check tik:
+            tik++;
+            if (tik >9)
+            {
+                tik = 0;
+                second++;
+            }
+            if (tik < 10)
+            {
+                lbTik.Text = "0" + tik;
+            }
+
+            else
+            {
+                lbTik.Text = tik + "";
+            }
+
+
+            //Check second:
+            if (second > 59)
+            {
+                second = 0;
+                minute++;
+
+            }
+
+            if (second < 10)
+            {
+                lbSecond.Text = "0" + second;
+            }
+            else
+            {
+                lbSecond.Text = second + "";
+            }
+
+
+
+            //Check Minutes:
+            if (minute > 59)
+            {
+                minute = 0;
+                hour++;
+            }
+
+            if (minute < 10)
+            {
+                lbMinute.Text = "0" + minute;
+            }
+            else
+            {
+                lbMinute.Text = minute + "";
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbTik.Text = "0";
+            lbSecond.Text = "00";
+            lbMinute.Text = "00";
+            lbHour.Text = "0";
+            timer1.Enabled = false;
+
+        }
+    }
+}
